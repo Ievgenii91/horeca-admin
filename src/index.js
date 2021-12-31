001f881addf8307795a9e02052ace731eaed3ab7
Yevhenii <Jimmydeanmaster@gmail.com>
2021-12-31 12:24:53 +0200

    js to jsx in components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.js';
+import App from './components/App';
 import { Provider } from 'react-redux';
 import store from './stores';
 import { BrowserRouter } from 'react-router-dom';
